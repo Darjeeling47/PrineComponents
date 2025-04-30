72c5fde975e61ceb69a3eee8dfb0a914ed698d4d
Supawit <supawit.ma@gmail.com>
2025-04-30 11:24:02 +0700

    feat: divider function

--- src/components/atoms/divider/Divider.tsx
+++ src/components/atoms/divider/Divider.tsx
@@ -1,24 +1,36 @@
 import clsx from "clsx";
 
-export default function Divider({ id, title }: DividerProps) {
+export default function Divider({ id, title, align }: DividerProps) {
   return (
     <div
       id={`divider-${id}`}
       className={clsx(
-        "my-2 flex w-full flex-row items-center justify-center gap-x-3",
+        "my-2 flex w-full flex-row items-center justify-center",
+        "text-nowrap font-semibold",
+        "text-gray-900",
       )}
     >
-      {title && (
-        <p className={clsx("text-nowrap font-semibold text-gray-900")}>
-          {title}
-        </p>
-      )}
-      <hr className={clsx("w-full", "border-gray-200")} />
+      <hr
+        className={clsx(
+          "w-full",
+          "border-gray-200",
+          align == "left" && "hidden",
+        )}
+      />
+      {title}
+      <hr
+        className={clsx(
+          "w-full",
+          "border-gray-200",
+          align == "right" && "hidden",
+        )}
+      />
     </div>
   );
 }
 
 interface DividerProps {
   id?: string;
-  title?: string;
+  title?: React.ReactNode;
+  align?: "left" | "center" | "right";
 }
